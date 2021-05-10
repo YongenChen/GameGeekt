ed2aecd3d99df4b9fa16fa75e5b7095b07623c1f
David Danialy <david.danialy@sjsu.edu>
2021-05-10 02:41:51 -0700

    Update Game.tsx

--- frontend/src/pages/dynamic/Game.tsx
+++ frontend/src/pages/dynamic/Game.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    minHeight: '100vh',
+    minHeight: '65vh',
     alignItems: 'center',
   },
   root: {
